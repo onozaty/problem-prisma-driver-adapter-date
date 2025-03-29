bf602f72eba346c7cb9124f2ca2dfef01e0250f3
onozaty <onozaty@gmail.com>
2025-03-29 22:20:52 +0900

    chnage with timezone

--- prisma/migrations/20250329123356_init/migration.sql
+++ prisma/migrations/20250329123356_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
